63dcdeaed1f9eece0791b0294e0a530b1f4a0beb
Nguyễn Thị Hồng Nhung <honggnhungg1605@gmail.com>
2025-10-06 23:43:30 +0700

    .upload source code

--- Profiles/MappingProfile.cs
+++ Profiles/MappingProfile.cs
@@ -2,31 +2,32 @@ using AutoMapper;
 using backend.Models;
 using backend.Dtos;
 
-<<<<<<< HEAD
+
+
 namespace backend.Profiles
-=======
-namespace backend.Mappings
->>>>>>> 98ae579 (upload source code)
+
+
 {
     public class MappingProfile : Profile
     {
         public MappingProfile()
         {
-<<<<<<< HEAD
+
             CreateMap<Class, ClassDto>()
                 .ForMember(dest => dest.StudentCount, opt => opt.MapFrom(src => src.Students.Count));
             CreateMap<CreateClassDto, Class>();
 
-            
+
             CreateMap<Student, StudentDto>()
                 .ForMember(dest => dest.ClassName, opt => opt.MapFrom(src => src.Class.Name));
             CreateMap<CreateStudentDto, Student>();
-=======
+
             CreateMap<Student, StudentDto>();
             CreateMap<CreateStudentDto, Student>();
             CreateMap<Class, ClassDto>();
             CreateMap<CreateClassDto, Class>();
->>>>>>> 98ae579 (upload source code)
+
         }
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
+
 using Microsoft.EntityFrameworkCore;
 using backend.Enity;
-
-=======
-
-using backend.Mappings;
 using AutoMapper;
 using backend;
->>>>>>> 98ae579 (upload source code)
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -17,13 +14,13 @@ builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(typeof(MappingProfile));
+//builder.Services.AddAutoMapper(typeof(MappingProfile));
 var app = builder.Build();
-<<<<<<< HEAD
 
-=======
+
+
 // Configure the HTTP request pipeline.
->>>>>>> 98ae579 (upload source code)
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -31,8 +28,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
-<<<<<<< HEAD
-=======
+
 AutoMapperTest.RunTest();
->>>>>>> 98ae579 (upload source code)
 app.Run();

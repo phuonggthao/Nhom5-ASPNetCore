dafeb523497071318fb8c5dd9326275c8ede65c9
Trương Thị Ánh <truongthianh23ct112@gmail.com>
2025-10-07 22:28:00 +0700

    Remove seed data from AppDbContext in main branch

--- Entity/AppDbContext.cs
+++ Entity/AppDbContext.cs
@@ -6,26 +6,8 @@ namespace backend.Entity
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        
+
         public DbSet<Class> Classes { get; set; } = null!;
         public DbSet<Student> Students { get; set; } = null!;
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Seed data cho bảng Class
-            modelBuilder.Entity<Class>().HasData(
-                new Class { Id = 1, Name = "Class A" },
-                new Class { Id = 2, Name = "Class B" }
-            );
-
-
-            // Seed data cho bảng Student
-            modelBuilder.Entity<Student>().HasData(
-                new Student { Id = 1, Name = "Vien Xuan Quy", DateOfBirth = new DateTime(2002, 5, 1), ClassId = 1 },
-                new Student { Id = 2, Name = "Truong Thi Anh", DateOfBirth = new DateTime(2001, 10, 20), ClassId = 2 }
-            );
-        }
     }
 }
-

dfd72b9d95935469937964d392b17d9bbdadd88b
Trương Thị Ánh <truongthianh23ct112@gmail.com>
2025-10-07 21:13:38 +0700

    Update AppDbContext and seed initial data

--- Entity/AppDbContext.cs
+++ Entity/AppDbContext.cs
@@ -6,9 +6,9 @@ namespace backend.Entity
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-    
+        
         public DbSet<Class> Classes { get; set; } = null!;
-public DbSet<Student> Students { get; set; } = null!;
+        public DbSet<Student> Students { get; set; } = null!;
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
